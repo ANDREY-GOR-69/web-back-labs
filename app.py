9aeb91f83efa1b5c31471219e45fdd5c0c0dbdce
Горшков Андрей <gorshkov5d4535@gmail.com>
2025-09-13 11:10:28 +0700

    Информация об авторе

--- app.py
+++ app.py
@@ -2,9 +2,26 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/web")
 def start():
-    return "<!doctype html>" \
-        "<html>" \
-        "   <body>" \
-        "       <h1>web-сервер на flask</h1>" \
-        "           </html>"
+    return '''<!doctype html>
+        <html>
+           <body>
+               <h1>web-сервер на flask</h1>
+                   </html>'''
+
+@app.route("/author")
+def author():
+    name = "Горшков Андрей Максимович"
+    group = "ФБИ-33"
+    faculty = "ФБ"
+
+    return '''<!doctype html>
+        <html>
+            <body>
+                <p>Студент: ''' + name + '''<p>
+                <p>Группа: ''' + group + '''<p>
+                <p>Факультет: ''' + faculty + '''<p>
+                <a href='/web'>web</a>
+            <body>
+        </html>'''
