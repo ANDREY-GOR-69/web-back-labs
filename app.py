7573e8ea2e62e036ad47f28302a812a2cd37bdac
Горшков Андрей <gorshkov5d4535@gmail.com>
2025-10-08 09:53:23 +0700

    Имя студента

--- app.py
+++ app.py
@@ -401,5 +401,6 @@ def add_flower(name):
 '''
 @app.route('/lab2/example')
 def example():
-    return render_template('example.html')
+    name = 'Горшков Андрей'
+    return render_template('example.html', name=name)
     
