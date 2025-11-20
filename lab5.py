cec20067a7ece7e4a8cd2a56bfa5dcabf6bc7e17
Горшков Андрей <gorshkov5d4535@gmail.com>
2025-11-20 09:52:42 +0700

    Список статей

--- lab5.py
+++ lab5.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, session
+from flask import Blueprint, render_template, request, session, redirect
 import psycopg2
 from psycopg2.extras import RealDictCursor
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -75,13 +75,34 @@ def register():
     db_close(conn, cur)
     return render_template('lab5/success.html', login=login)
 
+@lab5.route('/lab5/create', methods=['GET', 'POST'])
+def create():
+    login = session.get('login')
+    if not login:
+        return redirect('/lab5/login')
+    
+    if request.method == 'GET':
+        return render_template('lab5/create_article.html')
+    title = request.form.get('title')
+    article_text = request.form.get('article_text')
+    if not title or not article_text:
+        return render_template('lab5/create_article.html', error='Заполните все поля')
+
+    conn, cur = db_connect()
+
+    cur.execute("SELECT id FROM users WHERE login=%s;", (login,))
+    user = cur.fetchone()
+    login_id = user["id"]
+
+    cur.execute("INSERT INTO articles(user_id, title, article_text) VALUES (%s, %s, %s);", 
+                (login_id, title, article_text))
+
+    db_close(conn, cur)
+    return redirect('/lab5')
+
 @lab5.route('/lab5/list')
 def list_articles():
     return "Список статей"
-
-@lab5.route('/lab5/create')
-def create_article():
-    return "Создание статьи"
 
 @lab5.route('/lab5/logout')
 def logout():
